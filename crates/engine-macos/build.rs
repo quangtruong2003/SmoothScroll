0e38e1aa54ba64d726b00124700acb6bcad148aa
quangtruong2003 <nguyentruongk530042003@gmail.com>
2026-07-12 20:00:07 +0700

    fix(macos): link CoreGraphics unconditionally

--- crates/engine-macos/build.rs
+++ crates/engine-macos/build.rs
@@ -1,8 +1,5 @@
 fn main() {
-    #[cfg(target_os = "macos")]
-    {
-        println!("cargo:rustc-link-lib=framework=CoreGraphics");
-        println!("cargo:rustc-link-lib=framework=CoreFoundation");
-        println!("cargo:rustc-link-lib=framework=AppKit");
-    }
+    println!("cargo:rustc-link-lib=framework=CoreGraphics");
+    println!("cargo:rustc-link-lib=framework=CoreFoundation");
+    println!("cargo:rustc-link-lib=framework=AppKit");
 }
