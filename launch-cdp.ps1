c60e8a7a1107c0b3c595d74b86cb5e622a33755a
quangtruong2003 <nguyentruongk530042003@gmail.com>
2026-07-13 10:07:32 +0700

    chore: remove temp files and move dev scripts to scripts/
    - Delete temp_msg.txt (stale commit note)
    - Delete image.png (unused dot-grid texture)
    - Move launch-cdp.ps1, launch-with-cdp.bat, smoke-test.ps1 to scripts/

--- launch-cdp.ps1
+++
@@ -1,2 +0,0 @@
-$env:WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS = "--remote-debugging-port=9222"
-& "C:\Users\nguye\AppData\Local\SmoothScroll\smoothscroll-app.exe"
--- launch-with-cdp.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-set WEBVIEW2_ADDITIONAL_BROWSER_ARGUMENTS=--remote-debugging-port=9222
-start "" "C:\Users\nguye\AppData\Local\SmoothScroll\smoothscroll-app.exe"
